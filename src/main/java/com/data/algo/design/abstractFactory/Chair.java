db2dbbc0f18379ae45fcb76452aac89bb2afe15b
suthan-raja <suthanraja190@gmail.com>
2025-09-24 22:10:09 +0530

    Abstract Factory

---
+++ src/main/java/com/data/algo/design/abstractFactory/Chair.java
@@ -0,0 +1,56 @@
+package com.data.algo.design.abstractFactory;
+
+interface Chair {
+    void sitOn();
+}
+
+interface Sofa {
+    void lieOn();
+}
+
+class ModernChair implements Chair {
+    @Override
+    public void sitOn() { System.out.println("Sitting on Modern Chair"); }
+}
+
+class ModernSofa implements Sofa {
+    @Override
+    public void lieOn() { System.out.println("Lying on Modern Sofa"); }
+}
+
+class VictorianChair implements Chair {
+    @Override
+    public void sitOn() { System.out.println("Sitting on Victorian Chair"); }
+}
+
+class VictorianSofa implements Sofa {
+    @Override
+    public void lieOn() { System.out.println("Lying on Victorian Sofa"); }
+}
+
+interface FurnitureFactory {
+    Chair createChair();
+    Sofa createSofa();
+}
+
+class ModernFurnitureFactory  implements FurnitureFactory {
+    public Chair createChair() { return new ModernChair(); }
+    public Sofa createSofa() { return new ModernSofa(); }
+}
+
+class VictorianFurnitureFactory  implements FurnitureFactory {
+    public Chair createChair() { return new VictorianChair(); }
+    public Sofa createSofa() { return new VictorianSofa(); }
+}
+
+class Client {
+    public static void main(String[] args) {
+        FurnitureFactory furnitureFactory = new ModernFurnitureFactory();
+        furnitureFactory.createChair().sitOn();
+        furnitureFactory.createSofa().lieOn();
+
+        FurnitureFactory furnitureFactory2 = new VictorianFurnitureFactory();
+        furnitureFactory2.createChair().sitOn();
+        furnitureFactory2.createSofa().lieOn();
+    }
+}
